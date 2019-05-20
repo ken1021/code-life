49ab9d4dfc6909c803ea45a51b6ca15c3ab3109d
ken1021 <50267543+ken1021@users.noreply.github.com>
2019-05-20 20:55:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function pic(x){
 
 div= document.getElementById("a"+x).addEventListener("mouseover",a)
 
-    function a(){document.getElementById("a1pic").style="background-image:url(../image/a"+x+".jpg);background-size:cover;background-repeat:no-repeat;background-position:center; border:2px solid white;border-radius:15px;"}
+    function a(){document.getElementById("a1pic").style="background-image:url(image/a"+x+".jpg);background-size:cover;background-repeat:no-repeat;background-position:center; border:2px solid white;border-radius:15px;"}
 }
 
 for (i=1;i<35;i++){
